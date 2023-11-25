2173c454396af31986f67edb9c05538080d3933f
benchenchuang <benchenchuang>
2023-11-25 22:31:59 +0800

    req

--- src/app/api/system/users/route.ts
+++ src/app/api/system/users/route.ts
@@ -1,20 +1,25 @@
+/*
+ * @Author: benchenchuang benchenchuang
+ * @Date: 2023-11-25 08:06:11
+ * @LastEditors: benchenchuang benchenchuang
+ * @LastEditTime: 2023-11-25 22:31:49
+ * @FilePath: /next-app/src/app/api/system/users/route.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { connectToDB } from "@/libs/db";
 import { User } from "@/models/users";
 import { NextApiRequest } from "next/types";
 
-interface ResponseData {
-    code: number;
-    message: string;
-    data?: any
-}
-export const GET= async(req: NextApiRequest)=>{
+export const GET = async () => {
     try {
         connectToDB();
         const users = await User.find();
-        return Response.json({ data: {
-            list:users,
-            total:users.length
-        }, code: 200, message: '请求成功' })
+        return Response.json({
+            data: {
+                list: users,
+                total: users.length
+            }, code: 200, message: '请求成功'
+        })
     } catch (err: any) {
         console.log(err)
         // throw new Error('Failed to fetch users!');
