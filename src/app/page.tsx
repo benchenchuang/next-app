ff13a315bb78f5b7eaf24f97868a819366436221
benchenchuang <benchenchuang>
2023-11-25 23:17:46 +0800

    req

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,16 @@
-/*
- * @Author: benchenchuang benchenchuang
- * @Date: 2023-11-21 19:19:41
- * @LastEditors: benchenchuang benchenchuang
- * @LastEditTime: 2023-11-25 22:50:53
- * @FilePath: /next-app/src/app/page.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 'use client'
 import { useEffect } from 'react';
-// import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 const App = () => {
-    // const router = useRouter();
+    const router = useRouter();
     useEffect(() => {
-        // let token = '11';
-        // if (token) {
-        //     router.push('/views/dashboard')
-        // } else {
-        //     router.push('/login')
-        // }
+        let token = '11';
+        if (token) {
+            router.push('/views/dashboard')
+        } else {
+            router.push('/login')
+        }
     }, [])
 }
 
