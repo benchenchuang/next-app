402ca5dba0f766325c7f3237d388d711ef021f7a
benchenchuang <benchenchuang>
2023-11-25 22:40:52 +0800

    req

--- src/app/api/system/users/route.ts
+++ src/app/api/system/users/route.ts
@@ -2,15 +2,15 @@
  * @Author: benchenchuang benchenchuang
  * @Date: 2023-11-25 08:06:11
  * @LastEditors: benchenchuang benchenchuang
- * @LastEditTime: 2023-11-25 22:31:49
+ * @LastEditTime: 2023-11-25 22:40:43
  * @FilePath: /next-app/src/app/api/system/users/route.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { connectToDB } from "@/libs/db";
 import { User } from "@/models/users";
-import { NextApiRequest } from "next/types";
+import { NextRequest } from "next/server";
 
-export const GET = async () => {
+export const GET = async (req:Request | NextRequest) => {
     try {
         connectToDB();
         const users = await User.find();
