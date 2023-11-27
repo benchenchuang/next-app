5d15038771349db2a1faaeacb2d794002035875e
benchenchuang <790846384@qq.com>
2023-11-27 15:24:33 +0800

    prisma

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
   //     // appDir: true, //使用启用app目录
   //     urlImports: [], // 可使用三方地址，在引用css、img等位置需要配置
   // },
-  reactStrictMode: true, //是否使用严格模式
+  reactStrictMode: false, //是否使用严格模式
   publicRuntimeConfig: {}, // 导入全局配置，服务端和客户端都可以访问
   poweredByHeader: SERVER_ENV !== 'production', // 是否在head中增加Nextjs信息
   images: {
