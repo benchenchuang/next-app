ccd0d81cdd597793929c87b507b69b83f97fec37
benchenchuang <790846384@qq.com>
2023-11-28 12:47:52 +0800

    prisma

--- src/app/api/system/depart/route.ts
+++ src/app/api/system/depart/route.ts
@@ -2,10 +2,7 @@ import {prisma} from "@/libs/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async () => {
-    let data = await prisma.goods.findMany({
-        orderBy: {
-            createdTime: 'desc',
-        }
+    let data = await prisma.depart.findMany({
     });
     return NextResponse.json({
         success: true,
@@ -16,7 +13,7 @@ export const GET = async () => {
 
 export const POST = async(req:NextRequest)=>{
     const data = await req.json();
-    await prisma.goods.create({data,});
+    await prisma.depart.create({data,});
     return NextResponse.json({
         success:true,
         message:'创建成功',
