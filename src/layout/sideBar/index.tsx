9bc279dde11102512334ff50d3b811c16bd25f44
benchenchuang <790846384@qq.com>
2023-11-28 14:50:54 +0800

    user

--- src/layout/sideBar/index.tsx
+++ src/layout/sideBar/index.tsx
@@ -4,6 +4,7 @@ import {
     AppstoreOutlined,
     ContainerOutlined,
     DesktopOutlined,
+    SettingOutlined,
     MailOutlined,
     MenuFoldOutlined,
     MenuUnfoldOutlined,
@@ -32,41 +33,26 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('Option 3', '3', <ContainerOutlined />),
-
-    getItem('Navigation One', 'sub1', <MailOutlined />, [
-        getItem('Option 5', '5'),
-        getItem('Option 6', '6'),
-        getItem('Option 7', '7'),
-        getItem('Option 8', '8'),
-    ]),
-
-    getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-        getItem('Option 9', '9'),
-        getItem('Option 10', '10'),
-
-        getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
-    ]),
+    getItem('概览', 'dashboard', <DesktopOutlined />),
+    getItem('系统管理', 'system', <SettingOutlined />, [
+        getItem('用户管理', 'system/users'),
+        getItem('部门管理', 'system/depart'),
+        getItem('字典管理', 'system/dictionary'),
+    ])
 ];
 
 const LayoutSide = () => {
     let navigate = useRouter()
     const [collapsed, setCollapsed] = useState(false);
-    const [selectedKeys,setSelectedKeys] = useState('1');
+    const [selectedKeys, setSelectedKeys] = useState('1');
 
     const toggleCollapsed = () => {
         setCollapsed(!collapsed);
     };
 
-    const handleClickMenu = (e:any) =>{
-        let {key} = e;
-        if(key==1){
-            navigate.push('/admin/dashboard')
-        }else if(key==2){
-            navigate.push('/admin/system/menu')
-        }
+    const handleClickMenu = (e: any) => {
+        let { key } = e;
+        navigate.push('/admin/'+key)
         setSelectedKeys(key)
     }
 
