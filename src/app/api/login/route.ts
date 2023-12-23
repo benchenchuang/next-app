0eccbc6dd3dfa82fda8617479ca9f4492c696be4
benchenchuang <benchenchuang>
2023-12-23 17:25:20 +0800

    user

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,7 +2,7 @@
  * @Author: benchenchuang benchenchuang
  * @Date: 2023-12-01 19:15:44
  * @LastEditors: benchenchuang benchenchuang
- * @LastEditTime: 2023-12-23 17:14:11
+ * @LastEditTime: 2023-12-23 17:25:12
  * @FilePath: /next-app/src/app/api/login/route.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -37,8 +37,7 @@ export const POST = async (req: NextRequest) => {
         }
         //根据用户信息生成token返回
         let jwtToken = await signJWT(userInfo as any);
-        let permission = await getUserPermission(userInfo.roleId, '-1');
-        console.log(permission)
+        let permission: any = await getUserPermission(userInfo.roleId, '-1');
 
         return NextResponse.json(responseData(200, '登录成功', {
             token: jwtToken,
